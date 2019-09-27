701303a4f1b9ad36b97336ee4ee2ab086cdd3e17
chen <a1019289000@163.com>
2019-09-27 22:56:58 +0800

    0.01

---
+++ src/main/java/com/chen/controller/index.java
@@ -0,0 +1,12 @@
+package com.chen.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class index {
+    @RequestMapping("test")
+    public String x() {
+        return "test";
+    }
+}
