706933b5872ddc0d83217854907de35a54b594b2
chen <a1019289000@163.com>
2019-10-19 21:17:31 +0800

    0.1 database

--- src/main/java/com/chen/dao/bookimpl.java
+++ src/main/java/com/chen/dao/bookimpl.java
@@ -2,10 +2,12 @@ package com.chen.dao;
 
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface bookimpl {
     @Select("select * from book where name=#{name}")
     public book getbook(String name);
+    //    @Results
 }
--- src/test/java/com/chen/AppTest.java
+++ src/test/java/com/chen/AppTest.java
@@ -2,6 +2,7 @@ package com.chen;
 
 import static org.junit.Assert.assertTrue;
 
+import com.alibaba.fastjson.JSONObject;
 import org.junit.Test;
 
 /**
@@ -24,4 +25,25 @@ public class AppTest
             return "nn";
         }
     }
+    @Test
+    public void json(){
+        B b1=new B();
+        b1.b=100;
+        A aaa=new A();
+        aaa.a=111;
+        b1.aa=aaa;
+        String s=JSONObject.toJSONString(b1);
+        System.out.println(s);
+        B bb=JSONObject.parseObject(s,B.class);
+        System.out.println(bb);
+        System.out.println(bb.b);
+        System.out.println(bb.aa.a);
+    }
+}
+class A{
+    public int a;
+}
+class B{
+    public int b;
+    public A aa;
 }
